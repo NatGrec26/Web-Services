32eea13db1641feb95b1c94d25693fa185b04921
NatGrec26 <38171615+NatGrec26@users.noreply.github.com>
2018-06-04 22:55:38 -0600

    initial commit

---
+++ WebPruebaGiHub/WebPruebaGiHub/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebPruebaGiHub
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+
+        /// <summary>
+        /// /hola spy Natalia Probando 
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
